1b17c9543775717917a817d9037fe9a07e8198ca
Beatriz Ramos Rodrigues <beatriz.rodrigues@agilecontent.com>
2017-08-10 20:31:37 -0300

    Added functions

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-require('./app/index')
+const read = require('./app/readTxt');
+const dataArray = require('./app/dataArray');
+const result = read.readTxt();
+console.log(`The result is:\n${result}`);
+var array = dataArray.toDataArray(result);
+console.log(array);
